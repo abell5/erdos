970d075315f13e9a10f4d58e768d2ac56478bcf0
Andrew Bell <abell5@g.clemson.edu>
2016-04-03 00:08:35 -0400

    Add jquery to index.php and begin making textboxes

--- index.php
+++ index.php
@@ -1,20 +1,25 @@
 <?php
-
+//Convert \( \frac{4}{5} \) to a decimal.
 
 
 ?>
 
 <html>
 <head>
+<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
 <script type="text/javascript" async
   src="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-MML-AM_CHTML">
 </script>
-<script src="js/problem_obj.js"></script>
-</head>
 
 <body>
 
-Welcome to the platform<br>
+<div id="problemTextbox">test</div>
+<div id="answerBox">
+	<input type='text' style='width: 60px' name='answer'></input>
+</div>
+<div id="button">Blah</div>
 
 </body>
 </html>
+<script src="js/problem_obj.js"></script>
+</head>

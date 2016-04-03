5d4d425970c56b7c552f1e55895b320413973c6a
Andrew Bell <abell5@g.clemson.edu>
2016-04-03 00:45:46 -0400

    Add checkAnswer method for problem prototype

--- js/problem_obj.js
+++ js/problem_obj.js
@@ -1,22 +1,29 @@
 /*Will seperate into 2 files later, and implement RequireJS...*/
 /*For now, begin the class object...*/
 
-var Problem = function (text) {
+var Problem = function (text,answer) {
 	this.text = text;
+	this.answer = answer;
 	
 	console.log('Problem class instantiated');
 };
 
+Problem.prototype.checkAnswer = function(ans) {
+	if(this.answer == ans) {
+		console.log("correct");
+	} else {
+		console.log("Incorrect");
+	}
+}
 
 /*Begin the engine code...*/
 /*Engine methods*/
 function getInputValue(inputName) {
 	
 	var i = document.getElementsByName(inputName);
-	console.log(i);
 	
 	if (i[0].tagName == "INPUT" && i[0].type == "text") {
-		return i;
+		return i[0].value;
 	} else {
 		console.log("Non-input DOM element selected");
 	}
@@ -28,12 +35,14 @@ jQuery(function($) {
 	//Submit answer method
 	$("body").on("click", "#button", function() {
 		//Events that happen when div is clicked
-		console.log("clicked");
+		k = getInputValue("answer");
+		problem1.checkAnswer(k);
 	});
 	
-	getInputValue("answer");
 	
-	var problem1 = new Problem('Convert \\( \\frac{4}{5} \\) to a decimal.');
+	
+	var problem1 = new Problem('Convert \\( \\frac{4}{5} \\) to a decimal.'
+												,0.8);
 	
 	$('#problemTextbox').text(problem1.text); //Still unsure on whether to use .text() or .html()
 	

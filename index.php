9662b73b8c14620a619927cbdbd4f8a5b1a52de2
Andrew Bell <abell5@g.clemson.edu>
2016-04-02 22:46:00 -0400

    Add MathJax library

--- index.php
+++ index.php
@@ -6,6 +6,10 @@
 
 <html>
 <head>
+<script type="text/javascript" async
+  src="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-MML-AM_CHTML">
+</script>
+<script src="js/problem_obj.js"></script>
 </head>
 
 <body>

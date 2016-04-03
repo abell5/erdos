e4bc515d6122c760828c4bdb0143b8bcc83ba9c1
Andrew Bell <abell5@g.clemson.edu>
2016-04-03 00:07:50 -0400

    Add jquery and begin building engine

--- js/problem_obj.js
+++ js/problem_obj.js
@@ -1,5 +1,31 @@
-var Problem = new {
-	
-}
+/*Will seperate into 2 files later, and implement RequireJS...*/
+/*For now, begin the class object...*/
 
-var problem1 = new Problem;
+var Problem = function (text) {
+	this.text = text;
+	
+	console.log('Problem class instantiated');
+};
+
+
+/*Begin the engine code...*/
+/*Engine methods*/
+
+
+/*Main document code*/
+jQuery(function($) {
+	
+	//Submit answer method
+	$("body").on("click", "#button", function() {
+		//Events that happen when div is clicked
+		console.log("clicked");
+	});
+	
+	var problem1 = new Problem('Convert \\( \\frac{4}{5} \\) to a decimal.');
+	
+	$('#problemTextbox').text(problem1.text); //Still unsure on whether to use .text() or .html()
+	
+	
+});
+
+

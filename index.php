0d8f45516b365770c9f0c897a88d67bb6a7e351f
Andrew Bell <abell5@g.clemson.edu>
2016-04-05 01:12:51 -0400

    Change index.php

--- index.php
+++ index.php
@@ -9,15 +9,33 @@
 		<script type="text/javascript" async
 		  src="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-MML-AM_CHTML">
 		</script>
-		
-		<script data-main="requirejsConfig" src="js/lib/require.js"></script>
+		<script src="js/config.js"></script>
+		<script data-main="js/main.js" src="js/lib/require.js"></script>
 		<script>
-			/*
-			require(['common'],function() {
-				require(['problem_obj']);
+		
+			require(['js/main'], function() {
+				require(['problem_obj'], function(prob) {
+						//console.log(prob);
+						
+						var Problem = prob.getProblemObject();
+						
+						prob.world();
+						
+						var problem1 = new Problem(1
+												,'Convert \\( \\frac{4}{5} \\) to a decimal.'
+												,0.8
+												,[ [1.2, "displayHelperPlaintext('Close!  But I think you are mixing up the numerator and denominatory.'); displayHelperProblem(problem1)" ] ]
+												,"longForm"
+											  );
+											  
+					
+						});
+					
+			
 			});
-			*/
+		
 		</script>
+		
 	</head>
 
 

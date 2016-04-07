e3a54a6c27c901cf20f1d9c23b1b2edc223c3b4b
Andrew Bell <abell5@g.clemson.edu>
2016-04-06 23:52:11 -0400

    Update database login credentials

--- include/db_connect.php
+++ include/db_connect.php
@@ -4,7 +4,7 @@
 	define("MYSQL_DEFAULT_HOST", "localhost");
 	define("MYSQL_DEFAULT_USER", "root");
 	define("MYSQL_DEFAULT_PASS", "");
-	define("MYSQL_DEFAULT_DATABASE", "edu");
+	define("MYSQL_DEFAULT_DATABASE", "erdos");
 	
 	//Registration options
 	define("CAN_REGISTER", "any");

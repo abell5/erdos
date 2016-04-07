d1ec09ee1a4de0c98ef4910cb2f0450c4a4543cf
Andrew Bell <abell5@g.clemson.edu>
2016-04-06 23:20:21 -0400

    Add comment about difference between define() and require() for requirejs

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 *  This is the first JS file to be laoded into require JS, sets up paths
 */
 
+//General rule of thumb is you use define when you want to define a module that will be reused by your application and you use require to simply load a dependency.
 //Configure RequireJS
 
 requirejs.config({

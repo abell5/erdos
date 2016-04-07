175d8cf104b9cb51ac30b5bd38c5e018e065ce28
Andrew Bell <abell5@g.clemson.edu>
2016-04-06 23:22:47 -0400

    Create module_handler.js and begin module class

---
+++ js/module_handler.js
@@ -0,0 +1,16 @@
+ define(['jquery','probelm_obj'], function($) {
+	
+	var Problem = prob.getProblemObject();
+	
+	var Module = function(name,key_ids) {
+		this.name=name;
+		this.key_ids=key_ids;
+	}
+		
+	Module.prototype.create() {
+		forEach(function (entry) {
+			
+		});
+	}
+ 
+ });

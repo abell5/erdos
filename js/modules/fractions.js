f417e3bb0c5c9f02979c070cf07c5d57a3e6b591
Andrew Bell <abell5@g.clemson.edu>
2016-04-06 23:22:02 -0400

    Create fraction.js and display the module

--- js/modules/fractions.js
+++ js/modules/fractions.js
@@ -1,5 +1,28 @@
-require(['jquery','problem_obj'], function($) {
-	var problem1 = new Problem(1,1,1,1,1);
+require(['jquery','problem_obj'], function($, prob) {
+	
+	var Problem = prob.getProblemObject();
+	
+	branches = {
+		frac2dec: function () {
+			var helperProblem = new Problem(2, 'Hmm.. remember that we divide the numerator by the denominator.  Which number is the numerator, 4 or 5?', 4, null, "longForm");
+			var $helperTV = $( "#helperTV");
+			helperProblem.displayMe($helperTV);
+		}
+	}
+	
+	var problem1 = new Problem(1
+										,'Convert \\( \\frac{4}{5} \\) to a decimal.'
+										,0.8
+										,[ [1.2, "branches.frac2dec()" ] ]
+										,"longForm"
+									  );
+	
+	var $TV = $("#mainTV");
+	problem1.displayMe($TV);
+	
+});
+
+//var problem1 = new Problem(1,1,1,1,1);
 /*
 	function getInputValue(inputName) {
 		
@@ -30,4 +53,3 @@ require(['jquery','problem_obj'], function($) {
 	displayProblem(problem1);	
 		
 */
-});

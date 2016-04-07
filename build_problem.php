061a50dce1f94c356e09ed4cb8ac3644db42e458
Andrew Bell <abell5@g.clemson.edu>
2016-04-07 00:50:25 -0400

    Update build_problem and ajax call I was using the wrong variable name for id

--- build_problem.php
+++ build_problem.php
@@ -1,46 +1,41 @@
 <?php
 
-
-if(!isset($_GET['qid'])) {
+if(!isset($_GET['id'])) {
 	header('400 Bad Request');
 } else {
-	$qid = $_GET['qid'];
+	$id = $_GET['id'];
 	$data;
-	$mc;
 }
 
 
 try {
 	require('include/db_connect.php'); //Creates $DBH
 	
-	$query = "SELECT * FROM `ques` WHERE `id` = :id";
+	
+	$query = "SELECT * FROM `problems` WHERE `id` = :id";
 	$stmt = $DBH->prepare($query);
-	$stmt->bindValue(':id',$qid);
+	$stmt->bindValue(':id',$id);
 	
 	if($stmt->execute()) {
 		$rows = $stmt->fetch(PDO::FETCH_ASSOC);
-		if ($data['type']="mc") {
-			$data = array_merge($rows, get_mc($qid, $DBH));
-		} else {
-			$data = $rows;
-		}
+		$data = $rows;
 		//var_dump($data);
 	}
 	
-
 	echo json_encode($data);
-	
 	
 } catch(Exception $e) {
 	echo "Uh-oh";
 }
 
+
+/*
 function get_mc($mc_id, $DB) {
 
 	$query = "SELECT `value` FROM `mc` WHERE `q_id` = :qid";
 	$stmt = $DB->prepare($query);
 	$stmt->bindValue(':qid',$mc_id);
-	/*Get rows*/
+	//Get rows
 	if($stmt->execute()) {
 		$options = $stmt->fetchAll(PDO::FETCH_COLUMN);
 		$length = sizeof($options);
@@ -49,5 +44,5 @@ function get_mc($mc_id, $DB) {
 	}
 	
 }
-
+*/
 ?>

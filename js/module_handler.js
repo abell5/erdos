f199239fb35012fc6594b217ea43965c54228850
Andrew Bell <abell5@g.clemson.edu>
2016-05-04 22:27:36 -0400

    Add a huge comment section explaining the order in which all the callbacks fire.

--- js/module_handler.js
+++ js/module_handler.js
@@ -11,9 +11,29 @@
 		//which is stored as a variable as part of the object
 	}
 	
+	//Would be a great idea to write out the async sequence
+	/*  Async sequence
+	
+	1.  getProblem searches through its array of loaded problems (var problems)
+	2.  getProblem has a callback (CB1).  CB1 is the action that will be done when the problem is done being getted (lol).
+	3.  new Problem takes a callback (CB2).  CB2 is called by CB3, when new Problem is fully instantiated.  This operates at ALMOST the exact same level as CB1 (there are no async issues here), but its nice because it allows the problem to be stored.
+	[FUTURE WORK MAY BE COMBINING CB1 and CB2.  BUT FOR NOW THIS ALL WORKS].
+	4.  There is a callback hidden in the problem building class (CB3).  All this does is wait for the sql to finish before calling CB2.
+	
+	So in order:
+	1.  getProblem fires holding CB1 (the getted callback)
+	2.  (assuming no problem) new Problem fires holding CB2 (the deposited)
+	3.  build fires holding CB3 (the waiting callback)
+	4.  Sql occurs, causing a hold.
+	5.  CB3 fires causing CB2 to fires
+	6.  CB2 fires depositing the problem into the bank, and calls CB1
+	7.  CB1 fires and we are all gooooood.
+	*/
+	
 	Module.prototype.getProblem = function(id, callback) {
-		var out;
-		//console.log(this.problems);
+		var out; //shortened from out_problem ; but this is the problem object created in the if 
+					//statement below
+	
 		this.problems.forEach(function(entry) {
 			if (entry['id']==id) {
 				out = entry;

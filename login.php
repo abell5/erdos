16b98fcdf61e0412cb805e13c4d8b1adadd2ae24
Andrew Bell <abell5@g.clemson.edu>
2016-08-04 23:44:03 -0400

    Add valid email, available username, and matching passwords checks Restructure registerUser in a more logical fashion

--- login.php
+++ login.php
@@ -6,22 +6,22 @@
 ?>
 
 <html>
-
+<!--
 <form method="POST" action="">
 <input type="hidden" name="type" value=0/>
 Username:  <input type="text" name="user"/><br/>
 Password:  <input type="password" name="pass"/><br/>
 <input type="submit">
 </form>
-
+-->
 <br/>
-<hl>
 
 <form method="POST" action="registerUser.php">
 <input type="hidden" name="type" value=1/>
 Username:  <input type="text" name="user"/><br/>
 Password:  <input type="password" name="pass"/><br/>
 Confirm Password:  <input type="password" name="confirmPass"/><br/>
+E-mail:  <input type="text" name="email"/><br/>
 <input type="submit">
 </form>
 
--- registerUser.php
+++ registerUser.php
@@ -2,32 +2,49 @@
 require_once('include/db_connect.php');
 require_once('encryptionFunctions.php');
 
-if($_POST['pass'] == $_POST['confirmPass']) {
-	
+/**Retrieve posted variables **/
+$user = $_POST['user'];
+$email = $_POST['email'];
+$pass = $_POST['pass'];
+$confirmPass = $_POST['confirmPass'];
 
-	$query = "SELECT * FROM `users` WHERE LOWER(`username`) = :username";
-	$stmt = $DBH->prepare($query);
-	$stmt->bindValue(':username',strtolower($_POST['user']));
-	$stmt->execute();
+$errors = [];
+/* check e-mail */
+if(filter_var($email, FILTER_VALIDATE_EMAIL)!=$email) {
+	array_push($errors, "Please enter a valid e-mail address.");
+}
 
-	if($stmt->rowCount() > 0) {
-		echo "Username already exists.";
-	} else {
-		$query = "INSERT INTO `users` (`username`, `password`)
-					VALUES (:username, :password)";
-		$encryptedPassword = password_encrypt($_POST['pass']);
-		$stmt = $DBH->prepare($query);
-		$stmt->execute(array(":username" => $_POST['user'], ":password"=>$encryptedPassword));
-		
-		echo "User created.";
-		$_SESSION['user'] = $_POST['user'];
-		return true;
+/*Check if passwords match */
+if($pass != $confirmPass) {
+	array_push($errors, "One of those passwords is not like the other.");
+}
+
+/*Check if username already exists */
+$query = "SELECT * FROM `users` WHERE LOWER(`username`) = :username";
+$stmt = $DBH->prepare($query);
+$stmt->bindValue(':username',strtolower($user));
+$stmt->execute();
+
+if($stmt->rowCount() > 0) {
+	array_push($errors, "Someone beat you to that username.");
+}
+
+/*** End checks ***/
+
+if(!empty($errors)) {
+	foreach ($errors as $e) {
+		echo $e . "<br>";
 	}
-	
 } else {
+	$query = "INSERT INTO `users` (`username`, `password`)
+				VALUES (:username, :password)";
+	$encryptedPassword = password_encrypt($_POST['pass']);
+	$stmt = $DBH->prepare($query);
+	$stmt->execute(array(":username" => $_POST['user'], ":password"=>$encryptedPassword));
 	
-	echo "Passwords do not match.";
-	
+	echo "User created.";
+	$_SESSION['user'] = $_POST['user'];
+	return true;
 }
 
 ?>

05bf7e9d12296f0772e314012e2287c166d5edb7
Andrew Bell <abell5@g.clemson.edu>
2016-08-08 23:34:01 -0400

    Add get cut by difficulty to dashboard functionality

--- profile.php
+++ profile.php
@@ -13,7 +13,7 @@ if(isset($_SESSION['canary']['email'])) {
 }
 
 /********THIS IS A TEST STATEMENT, REMOVE THIS LATER************/
-$user = '0e4602da371c691fc2e348bf3f53a3e1';
+$user = 'ef82ad19df592dd26dc9e1e2b4563a63';
 
 echo $user;
 echo "<br><br>";
@@ -51,6 +51,62 @@ class Dashboard
 		if($stmt->execute(array(":user"=>$this->user, ":type"=>$type))) {
 				$rows = $stmt->fetchAll();
 				var_dump($rows);
+		}
+	}
+
+	public function getProblemsByDiff($diff) {
+		$query = "SELECT A.*, B.difficulty FROM `response_data` as A LEFT JOIN `sat_problems` as B
+						ON A.pid = B.id
+						WHERE `user_id` = :user AND `difficulty` = :diff";
+		$stmt = $this->DBH->prepare($query);
+		if($stmt->execute(array(":user"=>$this->user, ":diff"=>$diff))) {
+				$rows = $stmt->fetchAll();
+				var_dump($rows);
+		}
+	}
+
+	public function getCorrectByDiff($diff) {
+		$query = "SELECT A.*, B.difficulty FROM `response_data` as A LEFT JOIN `sat_problems` as B
+						ON A.pid = B.id
+						WHERE `user_id` = :user AND `difficulty` = :diff AND `response` = `correct_answer`";
+		$stmt = $this->DBH->prepare($query);
+		if($stmt->execute(array(":user"=>$this->user, ":diff"=>$diff))) {
+				$rows = $stmt->fetchAll();
+				var_dump($rows);
+		}
+	}
+	
+	public function getNumberByDiff($diff) {
+		$query = "SELECT count(`A`.`id`) as Num
+						FROM `response_data` as A LEFT JOIN `sat_problems` as B
+						ON A.pid = B.id
+						WHERE `user_id` = :user AND `difficulty` = :diff
+						GROUP BY `difficulty`";
+		$stmt = $this->DBH->prepare($query);
+		if($stmt->execute(array(":user"=>$this->user, ":diff"=>$diff))) {
+				$rows = $stmt->fetch();
+				if(empty($rows)) {
+					return 0;
+				} else {
+					return $rows['Num'];
+				}
+		}
+	}
+
+		public function getNumberCorrectByDiff($diff) {
+		$query = "SELECT count(`A`.`id`) as Num
+						FROM `response_data` as A LEFT JOIN `sat_problems` as B
+						ON A.pid = B.id
+						WHERE `user_id` = :user AND `difficulty` = :diff AND `response` = `correct_answer`
+						GROUP BY `difficulty`";
+		$stmt = $this->DBH->prepare($query);
+		if($stmt->execute(array(":user"=>$this->user, ":diff"=>$diff))) {
+				$rows = $stmt->fetch();
+				if(empty($rows)) {
+					return 0;
+				} else {
+					return $rows['Num'];
+				}
 		}
 	}
 	
@@ -95,8 +151,19 @@ class Dashboard
 			} else {
 				return -1;
 			}
-			return $percentage;
+			return $percentage;	
+	}
+
+	public function getPercentageByDiff($diff) {
+			$denominator = $this->getNumberByDiff($diff);
+			$numerator = $this->getNumberCorrectByDiff($diff);
 			
+			if($denominator!=0) {
+				$percentage = $numerator/$denominator;
+			} else {
+				return -1;
+			}
+			return $percentage;	
 	}
 	
 	public function softResponse($p) {
@@ -124,10 +191,21 @@ class Dashboard
 			return $rows;
 		}			
 	}
+
+	public function getListOfDiffs() {
+		$query = "SELECT DISTINCT `difficulty`
+						FROM `sat_problems`";
+		$stmt = $this->DBH->prepare($query);
+		if($stmt->execute()) {
+			$rows=$stmt->fetchAll(PDO::FETCH_COLUMN);
+			return $rows;
+		}			
+	}
 	
 }
 
-
+//$dashboard = new Dashboard($user, $DBH);
+//var_dump($dashboard->getListOfDiffs());
 ?>
 
 <html>
@@ -137,8 +215,8 @@ table {
     border-collapse: collapse;
     width: 100%;
 }
-
 th, td {
+
     text-align: left;
     padding: 8px;
 }
@@ -151,6 +229,25 @@ tr:nth-child(even){background-color: #f2f2f2}
 <body>
 <table style="width:60%" border="1">
 <?php
+
+$dashboard = new Dashboard($user, $DBH);
+
+$listOfDiffs = $dashboard->getListofDiffs();
+
+foreach($listOfDiffs as $diff) {
+	$perc = $dashboard->getPercentageByDiff($diff);
+	if($perc!=-1) {
+		echo "<tr>";
+			echo "<th>" . $diff . "</th>";
+			echo "<th>" . $perc . "</th>";
+			echo "<th>" . $dashboard->getNumberByDiff($diff) . "</th>";
+			echo "<th>" . $dashboard->softResponse($diff) . "</th>";
+		echo "</tr>";
+	}
+}
+
+
+/*
 $dashboard = new Dashboard($user, $DBH);
 
 $listOfTypes = $dashboard->getListofTypes();
@@ -166,7 +263,7 @@ foreach($listOfTypes as $type) {
 		echo "</tr>";
 	}
 }
-
+*/
 ?>
 </table>
 
